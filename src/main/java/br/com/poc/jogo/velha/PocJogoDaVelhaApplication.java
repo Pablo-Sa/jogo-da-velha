50e7320a9c0426031998e43987dfd2187c9a72f7
Pablo Pereira Sa <pablo.pereira.sa@everis.com>
2021-02-03 00:12:25 -0300

    Criado o Projeto

---
+++ src/main/java/br/com/poc/jogo/velha/PocJogoDaVelhaApplication.java
@@ -0,0 +1,13 @@
+package br.com.poc.jogo.velha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocJogoDaVelhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocJogoDaVelhaApplication.class, args);
+	}
+
+}
